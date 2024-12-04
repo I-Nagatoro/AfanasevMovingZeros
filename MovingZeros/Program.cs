bb9045705d15e3e8ab33cba10524270b74b42ed6
Alex <sokoldigg@ya.ru>
2024-12-04 13:04:44 +0300

    init

---
+++ MovingZeros/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MovingZeros
+{
+    class Program
+    {
+        static void MoveZeroes(int[] nums)
+        {
+            int notZero= 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[notZero] = nums[i];
+                    notZero++;
+                }
+            }
+            for (int i = notZero; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+            MoveZeroes(nums);
+            foreach (int i in nums)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
